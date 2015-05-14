711696d3a72409d0ca8c77403763710d11d03f08
Chris Oyler <christopher.oyler@gmail.com>
2015-05-13 19:32:43 -0700

    portfolio boxes work, moving things around

--- client/views/general/home.js
+++ client/views/general/home.js
@@ -1,10 +1,10 @@
 'use strict';
-$(document).ready(function(){
-  $('.materialboxed').materialbox();
-  $('.button-collapse').sideNav();
-});
+// $(document).ready(function(){
+//   $('.materialboxed').materialbox();
+//   $('.button-collapse').sideNav();
+// });
 
 angular.module('coyler')
 .controller('HomeCtrl', function(){
-  
+  angular.element('.materialboxed').materialbox();
 });
--- client/views/nav/nav.js
+++ client/views/nav/nav.js
@@ -1,8 +1,8 @@
 'use strict';
-$(document).ready(function(){
-  $('.materialboxed').materialbox();
-  $('.button-collapse').sideNav();
-});
+// $(document).ready(function(){
+//   $('.materialboxed').materialbox();
+//   $('.button-collapse').sideNav();
+// });
 // $('.collapsible').collapsible({
 //   //  accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
 // //  });
